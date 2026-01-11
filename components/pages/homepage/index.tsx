86743c071e8aab2541cbe79a279c80c35f11445f
Ezequiel Grigolatto <ezequiel@MacBook-Pro-de-Ezequiel.local>
2026-01-11 18:09:00 -0300

    adjusted lists height for mobile devices

--- components/pages/homepage/index.tsx
+++ components/pages/homepage/index.tsx
@@ -16,13 +16,13 @@ export default function HomePageComponent() {
   return (
     <div className="flex flex-col min-h-screen w-full justify-start bg-background text-foreground">
       <main className="flex-1 w-full flex flex-col items-center justify-start">
-        <h1 className="text-3xl font-bold mt-8 mb-4 px-4">
+        <h1 className="text-3xl font-bold mt-14 md:mt-8 mb-4 px-4">
           {t('HomePage.title')}
         </h1>
         <h2 className="text-lg mb-4 px-4 text-muted-foreground">
           {t('HomePage.subtitle')}
         </h2>
-        <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 px-4 mb-8 h-[40vh]">
+        <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 px-4 mb-8 h-[80vh] md:h-[40vh]">
           <CharacterList
             key="list1"
             listIndex={1}
