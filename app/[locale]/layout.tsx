d6481b153dc28e7e4d8ff8ec74b2882e4b209af0
Ezequiel Grigolatto <ezequiel@MacBook-Pro-de-Ezequiel.local>
2026-01-12 10:03:24 -0300

    metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -39,14 +39,15 @@ export default async function Layout({
   const JSON_LD = {
     '@context': 'https://schema.org',
     '@type': 'WebSite',
-    name: t('siteName'),
+    name: t('Metadata.siteName'),
     url: 'https://conexa-challenge.vercel.app',
-    description: t('siteDescription'),
+    description: t('Metadata.description'),
   };
 
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
+        <title>{t('Metadata.title')}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -62,9 +63,16 @@ export default async function Layout({
           hrefLang="en"
           href="https://conexa-challenge.vercel.app/en"
         />
-        <meta name="keywords" content={t('keywords')} />
+        <meta name="description" content={t('Metadata.description')} />
+        <meta name="keywords" content={t('Metadata.keywords')} />
         <meta name="author" content="Ezequiel Grigolatto" />
         <meta name="robots" content="noindex, nofollow" />
+        <meta property="og:title" content={t('Metadata.title')} />
+        <meta property="og:description" content={t('Metadata.description')} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://conexa-challenge.vercel.app" />
+        <meta property="og:site_name" content={t('Metadata.siteName')} />
+        <meta property="og:image" content="../../public/og-image.webp" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
