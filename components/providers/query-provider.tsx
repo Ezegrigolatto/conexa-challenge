27142aa1a1749a69885a60874ea5f3cfa1e232cb
Ezequiel Grigolatto <ezequiel@MacBook-Pro-de-Ezequiel.local>
2026-01-25 16:40:05 -0300

    feat: prefetch 1st character page

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -1,22 +1,27 @@
 'use client';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 0,
-        staleTime: 20000,
-        refetchOnWindowFocus: false,
-      },
-    },
-    queryCache: new QueryCache({
-      onError: (err) => {
-        console.error(err);
-      },
-    }),
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 0,
+            staleTime: 20000,
+            refetchOnWindowFocus: false,
+          },
+        },
+        queryCache: new QueryCache({
+          onError: (err) => {
+            console.error(err);
+          },
+        }),
+      })
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
